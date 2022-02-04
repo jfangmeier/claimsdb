cbda4ad6a83264dd41118ee56f60b8eed5b448d0
Josh Fangmeier <jfangmeier@gmail.com>
2022-02-04 08:26:02 -0600

    update data sample

--- data-raw/cms-synth-raw.R
+++ data-raw/cms-synth-raw.R
@@ -1,8 +1,8 @@
 library(tidyverse)
 library(httr2)
 
-codebook <- 
-  read_rds(here::here("data-raw", "codebook.rds")) %>% 
+codebook <-
+  read_rds(here::here("data-raw", "codebook.rds")) %>%
   mutate(
     COL_TYPE = case_when(
       VAR_TYPE == "string" ~ "c",
@@ -15,20 +15,20 @@ bene_years <- 2008:2009
 names(bene_years) <- 2008:2009
 
 walk(
-  .x = bene_years, 
-  ~paste0("https://www.cms.gov/Research-Statistics-Data-and-Systems/Downloadable-Public-Use-Files/SynPUFs/Downloads/DE1_0_", .x, "_Beneficiary_Summary_File_Sample_2.zip") %>% 
-    request() %>% 
-    req_retry(max_tries = 5) %>% 
+  .x = bene_years,
+  ~paste0("https://www.cms.gov/Research-Statistics-Data-and-Systems/Downloadable-Public-Use-Files/SynPUFs/Downloads/DE1_0_", .x, "_Beneficiary_Summary_File_Sample_2.zip") %>%
+    request() %>%
+    req_retry(max_tries = 5) %>%
     req_perform(path = here::here("data-raw", paste0("bene_", .x, ".zip")))
   )
 
-bene_cols <- 
-  codebook %>% 
-  filter(TABLE == "Bene") %>% 
-  pull(COL_TYPE) %>% 
+bene_cols <-
+  codebook %>%
+  filter(TABLE == "Bene") %>%
+  pull(COL_TYPE) %>%
   paste(., collapse = "")
 
-bene <- 
+bene <-
   map_dfr(
     .x = bene_years,
     ~read_csv(
@@ -38,111 +38,111 @@ bene <-
     .id = "BENE_YEAR"
   )
 
-# Select 200 random members from 2008 and keep their 2008-2009 beneficiary data
+# Select 500 random members from 2008 and keep their 2008-2009 beneficiary data
 set.seed(123)
-bene_rand <- 
-  bene %>% 
-  filter(BENE_YEAR == 2008) %>% 
-  sample_n(200) %>% 
+bene_rand <-
+  bene %>%
+  filter(BENE_YEAR == 2008) %>%
+  sample_n(500) %>%
   select(DESYNPUF_ID)
 
-bene_sample <- 
-  bene %>% 
+bene_sample <-
+  bene %>%
   inner_join(
-    bene_rand, 
+    bene_rand,
     by = "DESYNPUF_ID"
   )
 
 # For the sampled members, download and read their carrier, inpatient, outpatient, and pde data for 2008-2009
 walk(
-  .x = c("A", "B"), 
-  ~paste0("http://downloads.cms.gov/files/DE1_0_2008_to_2010_Carrier_Claims_Sample_2", .x, ".zip") %>% 
-    request() %>% 
-    req_retry(max_tries = 5) %>% 
+  .x = c("A", "B"),
+  ~paste0("http://downloads.cms.gov/files/DE1_0_2008_to_2010_Carrier_Claims_Sample_2", .x, ".zip") %>%
+    request() %>%
+    req_retry(max_tries = 5) %>%
     req_perform(path = here::here("data-raw", paste0("carrier_", .x, ".zip")))
 )
 
-carrier_cols <- 
-  codebook %>% 
-  filter(TABLE == "Carrier") %>% 
-  pull(COL_TYPE) %>% 
+carrier_cols <-
+  codebook %>%
+  filter(TABLE == "Carrier") %>%
+  pull(COL_TYPE) %>%
   paste(., collapse = "")
 
-carrier_sample <- 
+carrier_sample <-
   map_dfr(
     .x = c("A", "B"),
     ~read_csv(
       here::here("data-raw", paste0("carrier_", .x, ".zip")),
       col_types = carrier_cols,
       locale = locale(date_format = "%Y%m%d"))
-  ) %>% 
+  ) %>%
   inner_join(
     bene_rand, by = "DESYNPUF_ID"
-  ) %>% 
+  ) %>%
   filter_at(vars(CLM_FROM_DT, CLM_THRU_DT), all_vars(lubridate::year(.) %in% bene_years))
 
-"https://www.cms.gov/Research-Statistics-Data-and-Systems/Downloadable-Public-Use-Files/SynPUFs/Downloads/DE1_0_2008_to_2010_Inpatient_Claims_Sample_2.zip" %>% 
-  request() %>% 
-  req_retry(max_tries = 5) %>% 
+"https://www.cms.gov/Research-Statistics-Data-and-Systems/Downloadable-Public-Use-Files/SynPUFs/Downloads/DE1_0_2008_to_2010_Inpatient_Claims_Sample_2.zip" %>%
+  request() %>%
+  req_retry(max_tries = 5) %>%
   req_perform(path = here::here("data-raw", "inpatient.zip"))
 
-inpatient_cols <- 
-  codebook %>% 
-  filter(TABLE == "Inpatient") %>% 
-  pull(COL_TYPE) %>% 
+inpatient_cols <-
+  codebook %>%
+  filter(TABLE == "Inpatient") %>%
+  pull(COL_TYPE) %>%
   paste(., collapse = "")
 
-inpatient_sample <- 
+inpatient_sample <-
   read_csv(
     here::here("data-raw", "inpatient.zip"),
       col_types = inpatient_cols,
-      locale = locale(date_format = "%Y%m%d")) %>% 
+      locale = locale(date_format = "%Y%m%d")) %>%
   inner_join(
     bene_rand, by = "DESYNPUF_ID"
-  ) %>% 
+  ) %>%
   filter_at(vars(CLM_FROM_DT, CLM_THRU_DT), all_vars(lubridate::year(.) %in% bene_years))
 
-"https://www.cms.gov/Research-Statistics-Data-and-Systems/Downloadable-Public-Use-Files/SynPUFs/Downloads/DE1_0_2008_to_2010_Outpatient_Claims_Sample_2.zip" %>% 
-  request() %>% 
-  req_retry(max_tries = 5) %>% 
+"https://www.cms.gov/Research-Statistics-Data-and-Systems/Downloadable-Public-Use-Files/SynPUFs/Downloads/DE1_0_2008_to_2010_Outpatient_Claims_Sample_2.zip" %>%
+  request() %>%
+  req_retry(max_tries = 5) %>%
   req_perform(path = here::here("data-raw", "outpatient.zip"))
 
-outpatient_cols <- 
-  codebook %>% 
-  filter(TABLE == "Outpatient") %>% 
-  pull(COL_TYPE) %>% 
+outpatient_cols <-
+  codebook %>%
+  filter(TABLE == "Outpatient") %>%
+  pull(COL_TYPE) %>%
   paste(., collapse = "")
 
-outpatient_sample <- 
+outpatient_sample <-
   read_csv(
     here::here("data-raw", "outpatient.zip"),
     col_types = outpatient_cols,
-    locale = locale(date_format = "%Y%m%d")) %>% 
+    locale = locale(date_format = "%Y%m%d")) %>%
   inner_join(
     bene_rand, by = "DESYNPUF_ID"
-  ) %>% 
+  ) %>%
   filter_at(vars(CLM_FROM_DT, CLM_THRU_DT), all_vars(lubridate::year(.) %in% bene_years))
 
 
-"http://downloads.cms.gov/files/DE1_0_2008_to_2010_Prescription_Drug_Events_Sample_2.zip" %>% 
-  request() %>% 
-  req_retry(max_tries = 5) %>% 
+"http://downloads.cms.gov/files/DE1_0_2008_to_2010_Prescription_Drug_Events_Sample_2.zip" %>%
+  request() %>%
+  req_retry(max_tries = 5) %>%
   req_perform(path = here::here("data-raw", "pde.zip"))
 
-pde_cols <- 
-  codebook %>% 
-  filter(TABLE == "PDE") %>% 
-  pull(COL_TYPE) %>% 
+pde_cols <-
+  codebook %>%
+  filter(TABLE == "PDE") %>%
+  pull(COL_TYPE) %>%
   paste(., collapse = "")
 
-pde_sample <- 
+pde_sample <-
   read_csv(
     here::here("data-raw", "pde.zip"),
     col_types = pde_cols,
-    locale = locale(date_format = "%Y%m%d")) %>% 
+    locale = locale(date_format = "%Y%m%d")) %>%
   inner_join(
     bene_rand, by = "DESYNPUF_ID"
-  ) %>% 
+  ) %>%
   filter(lubridate::year(SRVC_DT) %in% bene_years)
 
 write_rds(bene_sample, here::here("data-raw", "bene.rds"))
